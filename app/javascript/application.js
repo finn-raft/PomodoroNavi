2eb72683e0a30026405924cc29321bf1040006a3
finn-raft <lemio3g9k@gmail.com>
2024-07-07 04:13:04 +0900

    ポモドーロタイマーの不具合修正 #23

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,4 +1,7 @@
 // Entry point for the build script in your package.json
+//= require turbolinks
 import "@hotwired/turbo-rails"
 import "./controllers"
+import Turbolinks from "turbolinks"
+Turbolinks.start()
 import * as bootstrap from "bootstrap"

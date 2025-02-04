9bc6295e227a8ffa598440328fd5d61ee519dcd0
finn-raft <lemio3g9k@gmail.com>
2025-02-04 21:36:09 +0900

    add: Google認証の実装(本番環境も動作確認済） #63

--- app/controllers/users/omniauth_callbacks_controller.rb
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -7,10 +7,20 @@ class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
   end
 
   def callback_for(provider)
-    # user.rbで記述したメソッド(from_omniauth)をここで使用
     @user = User.from_omniauth(request.env["omniauth.auth"])
-    sign_in_and_redirect @user, event: :authentication
-    set_flash_message(:notice, :success, kind: "#{provider}".capitalize) if is_navigational_format?
+    if @user.persisted?
+      sign_in @user, event: :authentication
+
+      if @user.navi_character_registered?
+        set_flash_message(:notice, :success, kind: "#{provider}".capitalize) if is_navigational_format?
+        redirect_to root_path
+      else
+        redirect_to new_navi_character_path
+      end
+    else
+      session["devise.#{provider}_data"] = request.env["omniauth.auth"].except(:extra) # Remove extra as it can overflow some session stores
+      redirect_to new_user_registration_url
+    end
   end
 
   def failure

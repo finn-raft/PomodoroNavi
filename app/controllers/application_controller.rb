9ea7443c9ecf9d03e71c75213e1980e780562fb6
finn-raft <lemio3g9k@gmail.com>
2025-02-06 21:55:03 +0900

    add: アプリのカラー変更機能 #76

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,6 @@
 class ApplicationController < ActionController::Base
   before_action :configure_permitted_parameters, if: :devise_controller?
+  before_action :set_pomodoro_settings
 
   def configure_permitted_parameters
     # 新規登録時にnameというキーのパラメーターを追加で許可する
@@ -8,5 +9,12 @@ class ApplicationController < ActionController::Base
     devise_parameter_sanitizer.permit(:sign_in, keys: [:attribute])
     # プロフィール更新時に許可するパラメーターを追加
     devise_parameter_sanitizer.permit(:account_update, keys: [:name, :profile])
+  end
+
+  private
+
+  # ユーザーのポモドーロタイマー設定を取得する
+  def set_pomodoro_settings
+    @pomodoro_settings = current_user&.pomodoro_setting || PomodoroSetting.default
   end
 end

bff5dd2b3fa235613d154c2dd8392439fe71303d
finn-raft <lemio3g9k@gmail.com>
2025-03-03 05:49:22 +0900

    fix: パスワード再設定ページのURLを独自ドメインに変更

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -79,12 +79,12 @@ Rails.application.configure do
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
   # config.action_mailer.raise_delivery_errors = false
   # 本番環境のメール送信設定
-  config.action_mailer.default_url_options = { host: 'https://pomodoro-navi.fly.dev/' }
+  config.action_mailer.default_url_options = { host: 'https://pomodoro-navi.com' }
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
     address:              'smtp.gmail.com',
     port:                 587,
-    domain:               'pomodoro-navi.fly.dev',
+    domain:               'pomodoro-navi.com',
     user_name:            ENV['MAILER_SENDER'],
     password:             ENV['MAILER_PASSWORD'],
     authentication:       'plain',
